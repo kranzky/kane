e0362825d49a5cef7c95fe340186aca005019627
Jason Hutchens <jasonhutchens@gmail.com>
2016-06-24 16:24:24 +0800

    added `#random`

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,8 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  def self.random(count=1)
+    query = self.limit(count).order("RANDOM()")
+    count == 1 ? query.first : query.to_a
+  end
 end
